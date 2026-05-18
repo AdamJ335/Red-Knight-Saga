7c1623a72a8e3a475b05656a3336f93454c3925d
AdamJ335 <adamj335@gmail.com>
2026-05-18 15:48:48 +0100

    adding player with basic movement

---
+++ Player/player.gd
@@ -0,0 +1,26 @@
+extends CharacterBody2D
+
+var movement_speed = 60.0
+
+@onready var sprite = $Sprite2D
+@onready var animation_player = $AnimationPlayer
+
+func _physics_process(delta: float) -> void:
+	movement()
+	
+func movement():
+	var x_mov = Input.get_action_strength("right") - Input.get_action_strength("left")
+	var y_mov = Input.get_action_strength("down") - Input.get_action_strength("up")
+	
+	var mov = Vector2(x_mov,y_mov)
+	if mov != Vector2.ZERO:
+		if mov.x < 0:
+			sprite.flip_h = true
+		if mov.x > 0:
+			sprite.flip_h = false
+		
+		velocity = mov.normalized()*movement_speed
+		move_and_slide()
+		animation_player.play("player_walk")
+	else:
+		animation_player.stop()
